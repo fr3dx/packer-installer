5f662de413dbe2c6e52d2adbf58d19a896528699
Ferenc Molnar <ferenc.m@protonmail.com>
2023-02-06 14:12:31 +0100

    first commit

---
+++ install_packer_1_8_5.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+<<comment
+ "This script downloads packer version 1.8.5, it checks the GPG signature
+  of packer_1.8.5_SHA256SUMS file, select the desired packer version 1.8.5 from it
+  and compares the selected hash with packer_1.8.5_linux_amd64.zip file"
+comment
+
+PACKER_DOWNLOAD_URL="https://releases.hashicorp.com/packer"
+PACKER_VERSION="1.8.5"
+PACKER_LINUX_BINARY="packer_${PACKER_VERSION}_linux_amd64.zip"
+
+wget ${PACKER_DOWNLOAD_URL}/${PACKER_VERSION}/${PACKER_LINUX_BINARY} &&
+wget ${PACKER_DOWNLOAD_URL}/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS &&
+wget ${PACKER_DOWNLOAD_URL}/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS.sig &&
+curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import &&
+gpg --verify packer_${PACKER_VERSION}_SHA256SUMS.sig packer_${PACKER_VERSION}_SHA256SUMS
+
+[ $? == 0 ] && echo "The signature is GOOD!"
+
+PACKER_BINARY_HASH=$(sha256sum ${PACKER_LINUX_BINARY} | awk '{ print $1 }')
+
+VERIFIED_PACKER_HASH=$(awk -e '/p.+linux_amd64.+/ {print $1}' packer_${PACKER_VERSION}_SHA256SUMS)
+
+[ "${VERIFIED_PACKER_HASH}" == "${PACKER_BINARY_HASH}" ] && echo "The hash is GOOD!"
+
+unzip ${PACKER_LINUX_BINARY} &&
+sudo mv packer /usr/bin
+
+packer --version
