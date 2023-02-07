b6580421ac47c5f2b8c38fb47509799c4822c525
Ferenc Molnar <ferenc.m@protonmail.com>
2023-02-07 09:15:09 +0100

    change logic

--- install_packer_1_8_5.sh
+++ install_packer_1_8_5.sh
@@ -14,17 +14,17 @@ wget ${PACKER_DOWNLOAD_URL}/${PACKER_VERSION}/${PACKER_LINUX_BINARY} &&
 wget ${PACKER_DOWNLOAD_URL}/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS &&
 wget ${PACKER_DOWNLOAD_URL}/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS.sig &&
 curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import &&
-gpg --verify packer_${PACKER_VERSION}_SHA256SUMS.sig packer_${PACKER_VERSION}_SHA256SUMS
-
-[ $? == 0 ] && echo "The signature is GOOD!"
+(gpg --verify packer_${PACKER_VERSION}_SHA256SUMS.sig packer_${PACKER_VERSION}_SHA256SUMS && echo "The signature is GOOD!") ||
+(echo "The signature is BAD!" && exit 1)
 
 PACKER_BINARY_HASH=$(sha256sum ${PACKER_LINUX_BINARY} | awk '{ print $1 }')
 
 VERIFIED_PACKER_HASH=$(awk -e '/p.+linux_amd64.+/ {print $1}' packer_${PACKER_VERSION}_SHA256SUMS)
 
-[ "${VERIFIED_PACKER_HASH}" == "${PACKER_BINARY_HASH}" ] && echo "The hash is GOOD!"
+([ "${VERIFIED_PACKER_HASH}" == "${PACKER_BINARY_HASH}" ] && echo "The hash is GOOD!") ||
+(echo "The hash is BAD!" && exit 1)
 
-unzip ${PACKER_LINUX_BINARY} &&
-mv packer /usr/bin
+#unzip ${PACKER_LINUX_BINARY} &&
+#mv packer /usr/bin
 
-packer --version
+#packer --version
